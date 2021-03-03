25c608ec5a43b20285021d19b4df0e2aa61d5541
Rodrigo Lima <rodrigolimasd@gmail.com>
2021-03-03 16:29:52 -0300

    add apiExceptionHandler

--- src/main/java/com/rodtech/qideasregisterapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/rodtech/qideasregisterapi/service/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.rodtech.qideasregisterapi.service.impl;
 
+import com.rodtech.qideasregisterapi.exception.RegisteredEmailException;
 import com.rodtech.qideasregisterapi.exception.UserNotFoundException;
 import com.rodtech.qideasregisterapi.model.User;
 import com.rodtech.qideasregisterapi.repository.UserRepository;
 import com.rodtech.qideasregisterapi.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static final String EMAIL_REGISTERED = "E-mail already registered";
 
     private final UserRepository userRepository;
 
@@ -22,12 +27,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByEmail(String email) {
-        return userRepository.findFirstByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
+        return findUserByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found"));
     }
 
     @Override
     public User create(User user) {
+        saveUserValidation(user);
         return userRepository.save(user);
     }
 
@@ -36,6 +41,8 @@ public class UserServiceImpl implements UserService {
         User userDb = findUserById(user.getId());
         //TODO: more information to update
         userDb.setEmail(user.getEmail());
+
+        saveUserValidation(userDb);
         userRepository.save(userDb);
         return null;
     }
@@ -46,8 +53,20 @@ public class UserServiceImpl implements UserService {
         userRepository.delete(user);
     }
 
+    private void saveUserValidation(User user){
+        findUserByEmail(user.getEmail())
+                .ifPresent(userDb -> {
+                    if(user.getId()==null || !user.getId().equals(userDb.getId()))
+                        throw new RegisteredEmailException(EMAIL_REGISTERED);
+                });
+    }
+
     private User findUserById(String id){
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    private Optional<User> findUserByEmail(String email){
+        return userRepository.findFirstByEmail(email);
     }
 }
